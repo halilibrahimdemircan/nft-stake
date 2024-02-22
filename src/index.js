70bb0ec73e0fb0c3f2b92297f33a3ff7896f3f7e
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2024-02-22 15:02:54 +0300

    dom fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { App } from "./App";
 import { Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
@@ -8,9 +8,12 @@ import "./index.css";
 function getLibrary(provider) {
   return new Web3Provider(provider);
 }
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+
+ReactDOM.render(
+  // @ts-ignore
+
   <Web3ReactProvider getLibrary={getLibrary}>
     <App />
-  </Web3ReactProvider>
+  </Web3ReactProvider>,
+  document.getElementById("app")
 );
